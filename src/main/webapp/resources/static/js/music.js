1e8c16adcb64f7426c3d8ec6cea02cfc3ae6631a
Andrew <andriypomin@gmail.com>
2022-09-25 19:43:46 +0300

    Test 4

--- src/main/webapp/resources/static/js/music.js
+++ src/main/webapp/resources/static/js/music.js
@@ -6,25 +6,27 @@ function startStop(element) {
     let song = document.getElementById(element);
     if (States.songPlaying !== null && !States.songPlaying.paused) {
         States.songPlaying.pause();
-        let templateId = States.songPlaying.id;
-        document.getElementById(templateId).classList.toggle("pause");
+        let templateId = "play_" + States.songPlaying.id;
+        document.getElementById("play_" + templateId).classList.toggle("pause");
     }
 
     if (song.paused) {
         States.songPlaying = song;
         song.play();
-        document.getElementById(element).classList.toggle("pause");
+        document.getElementById("play_" + element).classList.toggle("pause");
         song.onended = (event) => {
-            document.getElementById(element).classList.toggle("pause");
-            let next = song.nextSibling;
-            console.log(next)
+            document.getElementById("play_" + element).classList.toggle("pause");
+            let nextContainer = document.getElementById("play_" + element).parentElement.parentElement.nextSibling;
+            console.log(nextContainer);
+            let next = nextContainer.firstChild.firstChild;
+            console.log(next);
             let id = next.id;
             console.log(id);
             startStop(id);
         }
     } else {
         song.pause();
-        document.getElementById(element).classList.toggle("pause");
+        document.getElementById("play_" + element).classList.toggle("pause");
     }
 
     // switcher(song);
@@ -57,6 +59,7 @@ function startStop(element) {
 // }
 
 function getDuration(id) {
+    console.log("into");
     document.getElementById("info_" + id).innerText = document.getElementById(id).duration;
     console.log(document.getElementById(id).duration);
 }
