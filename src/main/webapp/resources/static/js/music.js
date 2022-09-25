cafaeffcda1514427b6b0549dbb5ebceccf30deb
Andrew <andriypomin@gmail.com>
2022-09-25 19:32:00 +0300

    Test 4

--- src/main/webapp/resources/static/js/music.js
+++ src/main/webapp/resources/static/js/music.js
@@ -6,38 +6,55 @@ function startStop(element) {
     let song = document.getElementById(element);
     if (States.songPlaying !== null && !States.songPlaying.paused) {
         States.songPlaying.pause();
-        States.songPlaying.classList.toggle("pause");
+        let templateId = States.songPlaying.id;
+        document.getElementById(templateId).classList.toggle("pause");
     }
 
-    switcher(song);
-}
-
-function switcher(song) {
     if (song.paused) {
         States.songPlaying = song;
         song.play();
-        song.classList.toggle("pause");
+        document.getElementById(element).classList.toggle("pause");
         song.onended = (event) => {
-            song.classList.toggle("pause");
+            document.getElementById(element).classList.toggle("pause");
             let next = song.nextSibling;
-            console.log(next);
-            startElement(next);
+            console.log(next)
+            let id = next.id;
+            console.log(id);
+            startStop(id);
         }
     } else {
-        States.songPlaying = null;
         song.pause();
-        song.classList.toggle("pause");
-    }
-}
-
-function startElement(element) {
-    if (States.songPlaying !== null && !States.songPlaying.paused) {
-        States.songPlaying.pause();
-        States.songPlaying.classList.toggle("pause");
+        document.getElementById(element).classList.toggle("pause");
     }
 
-    switcher(element);
+    // switcher(song);
 }
+
+// function switcher(song) {
+//     if (song.paused) {
+//         States.songPlaying = song;
+//         song.play();
+//         song.classList.toggle("pause");
+//         song.onended = (event) => {
+//             song.classList.toggle("pause");
+//             let next = song.nextSibling.id;
+//             console.log(next);
+//             startElement(next);
+//         }
+//     } else {
+//         song.pause();
+//         song.classList.toggle("pause");
+//     }
+// }
+//
+// function startElement(element) {
+//     if (States.songPlaying !== null && !States.songPlaying.paused) {
+//         States.songPlaying.pause();
+//         States.songPlaying.classList.toggle("pause");
+//     }
+//
+//     switcher(element);
+// }
 
 function getDuration(id) {
     document.getElementById("info_" + id).innerText = document.getElementById(id).duration;
